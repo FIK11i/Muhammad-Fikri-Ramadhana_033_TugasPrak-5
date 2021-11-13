1c445e94bf3219e26c45e49d278f184ffac6395f
Muhammad Fikri Ramadhana <90166829+FIK11i@users.noreply.github.com>
2021-11-13 22:10:02 +0700

    Add files via upload

---
+++ Muhammad Fikri Ramadhana_033_TugPrak5_nopro24.cpp
@@ -0,0 +1,21 @@
+//Progam 24 : Progam sederhana fungsi yang tidak mengembalikan nilai 
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void garis();
+main()
+{
+	system("cls");
+	garis();
+	printf("NO           NIM                NAMA                NILAI\n");
+	garis();
+	getch();
+
+}
+void garis()
+{
+	printf("========================================================== \n");
+}
+
+
